98607d387ee9efa045745ae1d1f1b588beda1a3b
masonturner <mason.turner@toolbox9.com>
2025-06-24 14:12:48 -0400

    Basic contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,17 @@ import { Heading, PageLayout } from '../_common/components';
 export default function About() {
   return (
     <PageLayout>
-      <Heading tag="h1">Contact</Heading>
-      <p className="text-lg">This is a placeholder for the contact page.</p>
+      <Heading tag="h1">Contact Mason</Heading>
+      <Heading tag="h2">E-mail</Heading>
+      <a href="mailto:msntrnrdev@gmail.com">msntrnrdev@gmail.com</a>
+      <Heading tag="h2">LinkedIn</Heading>
+      <a href="https://www.linkedin.com/in/msntrnr/" target="_blank" rel="noreferrer">
+        Mason&apos;s LinkedIn
+      </a>
+      <Heading tag="h2">Github</Heading>
+      <a href="http://github.com/tb9-mason" target="_blank" rel="noreferrer">
+        Mason&apos;s Github
+      </a>
     </PageLayout>
   );
 }
