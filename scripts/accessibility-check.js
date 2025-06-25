65cb5728b613e9a70ba9aeb56fd7ce70c6de54e4
masonturner <mason.turner@toolbox9.com>
2025-06-25 13:43:42 -0400

    Adds dark mode toggle action to every page. Reduces repetition of the chromeLaunchConfig option

--- scripts/accessibility-check.js
+++ scripts/accessibility-check.js
@@ -1,15 +1,18 @@
 const pa11y = require('pa11y');
 
+const DEFAULT_PA11Y_OPTIONS = { chromeLaunchConfig: { args: ['--no-sandbox'] } };
+const DEFAULT_PAGE_ACTIONS = ['click element .theme-toggle'];
+
 const checks = [
-  { url: 'localhost:3000', options: { actions: ['click element .theme-toggle'] } },
-  { url: 'localhost:3000/projects', options: { actions: [] } },
-  { url: 'localhost:3000/contact', options: { actions: [] } },
+  { url: 'localhost:3000', options: { actions: DEFAULT_PAGE_ACTIONS } },
+  { url: 'localhost:3000/projects', options: { actions: DEFAULT_PAGE_ACTIONS } },
+  { url: 'localhost:3000/contact', options: { actions: DEFAULT_PAGE_ACTIONS } },
 ];
 
 (async () => {
   for (const check of checks) {
     console.log(`\nChecking ${check.url}`);
-    const results = await pa11y(check.url, { chromeLaunchConfig: { args: ['--no-sandbox'] } });
+    const results = await pa11y(check.url, DEFAULT_PA11Y_OPTIONS);
 
     if (results.issues.length) {
       console.error(`Issues found on ${check.url}`);
@@ -22,7 +25,7 @@ const checks = [
     if (check.options.actions.length) {
       console.log(`${check.url} has ${check.options.actions.length} actions, checking...`);
       const actionResults = await pa11y(check.url, {
-        chromeLaunchConfig: { args: ['--no-sandbox'] },
+        ...DEFAULT_PA11Y_OPTIONS,
         ...check.options,
       });
 
