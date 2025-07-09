19596ce10d4aa49c746d20dacda6d8ed2515c8a3
masonturner <msntrnrdev@gmail.com>
2025-07-09 11:29:31 -0400

    Updated copy for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,10 +7,14 @@ export default function Examples() {
     <PageLayout>
       <Heading tag="h1">Mason&apos;s Projects</Heading>
       <p className="text-lg">
-        Here&apos;s a few notable projects I&apos;ve worked on. These are all in private repositories, so unfortunately
-        I can&apos;t share code. If you want to see some of my personal work, you can go{' '}
+        Here&apos;s a few notable projects I&apos;ve worked on. If you want to see some of my personal work, you can
+        visit{' '}
         <a href="https://demo.msntrnr.com" target="_blank" rel="noreferrer">
-          here.
+          my demo site
+        </a>{' '}
+        or{' '}
+        <a href="https://www.github.com/tb9-mason" target="_blank" rel="noreferrer">
+          my GitHub.
         </a>
       </p>
       {projects.map((p, index) => {
